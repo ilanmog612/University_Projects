197bfd566bb70230b878db7848f7b3a62b365b85
Mamat <ilanmog612@gmail.com>
2021-04-09 16:38:02 +0300

    done with explenation

--- HW_1/scrape_players.sh
+++ HW_1/scrape_players.sh
@@ -6,16 +6,17 @@ cat players | grep -P "/players/[0-9]+/[a-z A-z -]+/overview" > out.text
 
 sed -i 's/\/players/https:\/\/www\.premierleague\.com\/players/' out.text
 
-players_num=$(cat out.text | wc -l)
-echo "total players: $players_num" > results.csv
+players_num=$(cat out.text | wc -l) #total number of lines we got from the premierleague site.
+echo "total players: $players_num" > results.csv #setting up the csv file with an headline
 
-for (( i=1; i<=$players_num; ++i )); do
+for (( i=1; i<=$players_num; ++i )); do # this loop fills the table 
 
-link=$(cat out.text | head -n $i | tail -n 1 | grep -P -o "https://www.premierleague.com/players/[0-9]+/[a-z A-z -]+/overview"  )
+#getting the links
+link=$(cat out.text | head -n $i | tail -n 1 | grep -P -o "https://www.premierleague.com/players/[0-9]+/[a-z A-z -]+/overview"  ) 
 wget "$link" 
-num_of_times=$(cat overview | grep -P -o "[Mm]idfielder" | wc -w )
+num_of_times=$(cat overview | grep -P -o "[Mm]idfielder" | wc -w ) # counting how many times the word midfielder was in the players page
 
-if [[ $num_of_times -gt 0 ]]; then 
+if [[ $num_of_times -gt 0 ]]; then #if the nummber is bigger then zero the player will go to the results table
 
 echo "$link, Midfielder, $num_of_times" >> results.csv
 
