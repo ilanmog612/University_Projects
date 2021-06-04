3d577ce7249c67412e8df38f8311d079c3fcd7fa
Mamat <ilanmog612@gmail.com>
2021-06-04 15:35:57 +0300

    done

---
+++ HW_6/port.h
@@ -0,0 +1,29 @@
+#ifndef PORT_H
+#define PORT_H
+
+
+#include "field.h"
+
+class Port: public Field
+{
+private :
+    String pattern;
+
+
+public:
+    /**
+        * @brief low port num and high port num
+        */
+        int low_value;
+        int high_value;
+     Port (String pattern);
+     /**
+         * @brief sets low_value & high_value
+         */
+    bool set_value(String val);
+    /**
+         * @brief Check if packet matches the rule given by pattern
+         */
+    bool match_value(String packet) const;
+};
+#endif
