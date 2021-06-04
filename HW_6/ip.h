5f873e3878d48e3eedd7b5de88d4c676bc5950e6
Mamat <ilanmog612@gmail.com>
2021-06-04 15:34:56 +0300

    done

---
+++ HW_6/ip.h
@@ -0,0 +1,32 @@
+#ifndef IP_H
+#define IP_H
+
+
+#include "field.h"
+#define ip_SIZE 32
+
+class ip: public Field
+{
+private :
+    String pattern;
+
+public:
+    /**
+        * @brief num of bits to consider in rule
+        */
+    int offset;
+    /**
+         * @brief bit array for ip address
+         */
+    int bit_arr[ip_SIZE];
+     ip (String pattern);
+     /**
+          * @brief set offset
+          */
+    bool set_value(String val);
+    /**
+        * @brief Check if packet matches the rule given by pattern
+        */
+    bool match_value(String packet) const;
+};
+#endif
